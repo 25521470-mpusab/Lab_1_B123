d39687e89b1a200de56c92b713252a17391f59e4
Phương Lê Ngọc Mai <25521470@gm.uit.edu.vn>
2026-03-11 23:30:21 +0700

    Update Lab1_B123.cpp

--- B123/Lab1_B123.cpp
+++ B123/Lab1_B123.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
 using namespace std;
 
-// Struct PhanSo
-struct PhanSo{
-int tu;
-int mau;
-};
+/* Hàm nhập phân số
+Input: tử số t, mẫu số m
+Output: giá trị t và m được nhập từ bàn phím */
+void Nhap(int &t, int &m){
+    cin >> t >> m;
+}
+/* Hàm tìm UCLN
+Input: t, m
+Output: ước chung lớn nhất của t và m */
+int UCLN(int a, int b){
+    a = abs(a);
+    b = abs(b);
 
-//Hàm g = UCLN
-int g(int a,int b){
-  wh
+    while(b != 0){
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+/*
+Hàm rút gọn phân số
+Input: t, m
+Output: phân số đã rút gọn
+*/
+void RutGon(int &t, int &m){
+
+    int g = UCLN(t,m);
+
+    t = t / g;
+    m = m / g;
+
+    // chuẩn hóa dấu
+    if(m < 0){
+        t = -t;
+        m = -m;
+    }
+}
+/*
+Hàm xuất phân số
+Input: t, m
+Output: in phân số ra màn hình
+*/
+void Xuat(int t, int m){
+    cout << t << "/" << m;
+}
+
+int main(){
+
+    int t, m;
+
+    Nhap(t,m);
+
+    if(m == 0){
+        cout << "Mau so khong hop le";
+        return 0;
+    }
+
+    RutGon(t,m);
+
+    Xuat(t,m);
+
+    return 0;
+}
