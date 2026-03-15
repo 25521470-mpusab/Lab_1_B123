21dec7ba69f8a0206b9b8008d1aebafaea88a301
Phương Lê Ngọc Mai <25521470@gm.uit.edu.vn>
2026-03-15 22:47:51 +0700

    Update Lab1_B4.cpp

--- B4/Lab1_B4.cpp
+++ B4/Lab1_B4.cpp
@@ -1,53 +1,89 @@
 #include <iostream>
 using namespace std;
 
-struct Date{
+/*
+Struct Ngay
+Lưu trữ thông tin một ngày gồm:
+- ngay
+- thang
+- nam
+*/
+struct Ngay{
     int ngay;
     int thang;
     int nam;
 };
 
+/*
+Hàm kiểm tra năm nhuận
+Input: nam
+Output: true nếu là năm nhuận, false nếu không
+*/
 bool ktraNamNhuan(int nam){
     if(nam % 400 == 0) return true;
     if(nam % 4 == 0 && nam % 100 != 0) return true;
     return false;
 }
 
+/*
+Hàm trả về số ngày trong một tháng
+Input: thang, nam
+Output: số ngày của tháng đó
+*/
 int NgayMay(int thang, int nam){
     switch(thang){
         case 1: case 3: case 5: case 7:
         case 8: case 10: case 12:
             return 31;
+
         case 4: case 6: case 9: case 11:
             return 30;
+
         case 2:
-            return ktraNamNhuan(nam) ? 29 : 28;
+            if(ktraNamNhuan(nam)) return 29;
+            else return 28;
     }
     return 0;
 }
 
-void addDate(Date &d, int numDays){
-    d.ngay += numDays;
+/*
+Hàm cộng thêm một số ngày vào ngày ban đầu
+Input:
+- d: ngày ban đầu
+- soNgayThem: số ngày cần cộng
+
+Output:
+- cập nhật lại ngày mới
+*/
+void themNgay(Ngay &d, int soNgayThem){
+
+    d.ngay += soNgayThem;
 
     while(d.ngay > NgayMay(d.thang, d.nam)){
         d.ngay -= NgayMay(d.thang, d.nam);
         d.thang++;
 
-        while(d.thang > 12){
-            d.thang -= 12;
+        if(d.thang > 12){
+            d.thang = 1;
             d.nam++;
         }
     }
 }
 
 int main(){
-    Date a;
+
+    Ngay a;
+
+    // Nhập ngày ban đầu
     cin >> a.ngay >> a.thang >> a.nam;
 
-    int days;
-    cin >> days;
+    int soNgay;
+    cin >> soNgay;
 
-    addDate(a, days);
+    // Cộng thêm ngày
+    themNgay(a, soNgay);
+
+    // Xuất kết quả
     cout << a.ngay << "/" << a.thang << "/" << a.nam;
 
     return 0;
