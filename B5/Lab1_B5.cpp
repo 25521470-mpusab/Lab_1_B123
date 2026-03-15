97b9377c22247bc86309c4860fa7c4b628a3d022
Phương Lê Ngọc Mai <25521470@gm.uit.edu.vn>
2026-03-15 22:41:37 +0700

    Update Lab1_B5.cpp

--- B5/Lab1_B5.cpp
+++ B5/Lab1_B5.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
 
+struct HocSinh{
+    string hoten;
+    float toan;
+    float van;
+};
+
+int main(){
+
+    HocSinh hs;
+
+    cout<<"Nhap ho ten: ";
+    getline(cin,hs.hoten);
+
+    cout<<"Nhap diem toan: ";
+    cin>>hs.toan;
+
+    cout<<"Nhap diem van: ";
+    cin>>hs.van;
+
+    float dtb=(hs.toan+hs.van)/2;
+
+    cout<<"Ho ten: "<<hs.hoten<<endl;
+    cout<<"Diem trung binh: "<<dtb;
+
+}
