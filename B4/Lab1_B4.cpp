4fb1214cbdc90a8db5625cfd615970c186f5f142
Phương Lê Ngọc Mai <25521470@gm.uit.edu.vn>
2026-03-15 22:44:01 +0700

    Update Lab1_B4.cpp

--- B4/Lab1_B4.cpp
+++ B4/Lab1_B4.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
 using namespace std;
- 
+
 struct Date{
-    int day;
-    int month;
-    int year;
+    int ngay;
+    int thang;
+    int nam;
 };
 
-bool ktraNamNhuan(int year){
-    if( year % 400 == 0) return true;
-    if( year % 4 == 0 && year % 100 != 0) return true;
+bool ktraNamNhuan(int nam){
+    if(nam % 400 == 0) return true;
+    if(nam % 4 == 0 && nam % 100 != 0) return true;
     return false;
 }
 
-int NgayTrongThang( int month, int year){
-    switch(month){
+int NgayMay(int thang, int nam){
+    switch(thang){
         case 1: case 3: case 5: case 7:
         case 8: case 10: case 12:
             return 31;
+
         case 4: case 6: case 9: case 11:
             return 30;
+
         case 2:
-            return ktraNamNhuan(year) ? 29 : 28;
- 
+            return ktraNamNhuan(nam) ? 29 : 28;
     }
+    return 0;
 }
 
 void addDate(Date &d, int numDays){
-    d.day += numDays;
-    while(d.day > NgayTrongThang(d.month, d.year)){
-        d.day -= NgayTrongThang(d.month, d.year);
-        d.month ++;
-   
-     while (d.month > 12){
-        d.month -= 12;
-        d.year ++;
+    d.ngay += numDays;
+
+    while(d.ngay > NgayMay(d.thang, d.nam)){
+        d.ngay -= NgayMay(d.thang, d.nam);
+        d.thang++;
+
+        while(d.thang > 12){
+            d.thang -= 12;
+            d.nam++;
         }
     }
 }
- 
-int main()
-{
+
+int main(){
     Date a;
-    cin>> a.day>> a.month >> a.year;
+
+    cin >> a.ngay >> a.thang >> a.nam;
+
     int days;
-    cin>> days;
+    cin >> days;
+
     addDate(a, days);
-    cout<< a.day<< "/"<< a.month<<"/"<< a.year;
- 
+
+    cout << a.ngay << "/" << a.thang << "/" << a.nam;
+
     return 0;
 }
