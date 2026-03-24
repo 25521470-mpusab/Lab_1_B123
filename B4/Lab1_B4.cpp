ef5bf8fa371322a147d1b6059a129728482b0c66
Phương Lê Ngọc Mai <25521470@gm.uit.edu.vn>
2026-03-24 22:45:18 +0700

    Update Lab1_B4.cpp

--- B4/Lab1_B4.cpp
+++ B4/Lab1_B4.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-struct Ngay{
+struct Ngay {
     int ngay;
     int thang;
     int nam;
 };
 
-bool ktraNamNhuan(int nam){
-    if(nam % 400 == 0) return true;
-    if(nam % 4 == 0 && nam % 100 != 0) return true;
+// kiểm tra năm nhuận
+bool ktraNamNhuan(int nam) {
+    if (nam % 400 == 0) return true;
+    if (nam % 4 == 0 && nam % 100 != 0) return true;
     return false;
 }
 
-int NgayMay(int thang, int nam){
-    switch(thang){
+// số ngày trong tháng
+int soNgayTrongThang(int thang, int nam) {
+    switch (thang) {
         case 1: case 3: case 5: case 7:
         case 8: case 10: case 12:
             return 31;
@@ -23,34 +25,51 @@ int NgayMay(int thang, int nam){
             return 30;
 
         case 2:
-            if(ktraNamNhuan(nam)) return 29;
+            if (ktraNamNhuan(nam)) return 29;
             else return 28;
     }
     return 0;
 }
 
-void ngayKeTiep(Ngay &d){
+// kiểm tra ngày hợp lệ
+bool KiemTraNgay(Ngay d) {
+    if (d.thang < 1 || d.thang > 12) return false;
+    if (d.ngay < 1 || d.ngay > soNgayTrongThang(d.thang, d.nam)) return false;
+    return true;
+}
 
+// tính ngày kế tiếp
+void ngayKeTiep(Ngay &d) {
     d.ngay++;
 
-    if(d.ngay > NgayMay(d.thang, d.nam)){
+    if (d.ngay > soNgayTrongThang(d.thang, d.nam)) {
         d.ngay = 1;
         d.thang++;
 
-        if(d.thang > 12){
+        if (d.thang > 12) {
             d.thang = 1;
             d.nam++;
         }
     }
 }
 
-int main(){
-
+int main() {
     Ngay a;
+
+    cout << "Nhap ngay thang nam: ";
     cin >> a.ngay >> a.thang >> a.nam;
 
+    // kiểm tra hợp lệ
+    if (!KiemTraNgay(a)) {
+        cout << "Ngay khong hop le!";
+        return 0;
+    }
+
+    // tính ngày kế tiếp
     ngayKeTiep(a);
 
+    cout << "Ngay ke tiep: ";
     cout << a.ngay << "/" << a.thang << "/" << a.nam;
+
     return 0;
 }
